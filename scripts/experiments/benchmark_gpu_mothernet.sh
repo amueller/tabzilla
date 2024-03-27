eac9871b3529786ed086dd5f5349324a75748584
Andreas Mueller <amueller@microsoft.com>
2024-03-27 10:11:41 -0700

    add mothernet with 1000 sample subset

--- scripts/experiments/benchmark_gpu_mothernet.sh
+++ scripts/experiments/benchmark_gpu_mothernet.sh
@@ -7,15 +7,15 @@ source ../utils.sh
 # this defines DATASETS
 source ../HARD_DATASETS_BENCHMARK.sh
 
-name=bench-mothernet-gpu
-model=mothernet
+name=bench-mothernet-gpu-1000
+model=mothernet-1000
 
 
 # experiment name (will be appended to results files)
 experiment_name=$name
 
 # config file
-config_file=../../TabZilla/tabzilla_experiment_config_mothernet_gpu_subset.yml
+config_file=../../TabZilla/tabzilla_experiment_config_mothernet_gpu_subset_1000.yml
 
 # end: EXPERIMENT PARAMETERS
 ############################
@@ -42,7 +42,7 @@ do
   echo "DATASET: ${DATASETS[j]}"
   echo "EXPERIMENT_NAME: ${experiment_name}"
   #echo "tabzilla_experiment.py --experiment_config ${config_file} --dataset_dir datasets/${DATASETS[j]} --model_name MotherNet "
-  python ../../TabZilla/tabzilla_experiment.py --experiment_config ${config_file} --dataset_dir ../../TabZilla/datasets/${DATASETS[j]} --model_name MotherNet
+  python ../../TabZilla/tabzilla_experiment.py --experiment_config ${config_file} --dataset_dir ../../TabZilla/datasets/${DATASETS[j]} --model_name MotherNet1000
   mv results/default_trial0_results.json results/results_${DATASETS[j]}_mothernet.json
 
   # >> ${LOG_DIR}/log_${i}_${j}_$(date +"%m%d%y_%H%M%S").txt 2>&1 &                                                                                                   
