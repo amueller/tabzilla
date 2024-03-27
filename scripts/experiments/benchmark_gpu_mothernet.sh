7b6e2770e589f8b0aa3182f95d6f916c7599d4a5
Andreas Mueller <amueller@microsoft.com>
2024-03-26 19:11:28 -0700

    cleanup and fix path

--- scripts/experiments/benchmark_gpu_mothernet.sh
+++ scripts/experiments/benchmark_gpu_mothernet.sh
@@ -40,25 +40,12 @@ num_experiments=0
 for j in ${!DATASETS[@]};
 do
 
-  # if the experiment is already in the result log, skip it
-  if grep -Fxq "${DATASETS[j]},${model},${experiment_name}" ${result_log}; then
-    echo "experiment found in logs. skipping. dataset=${DATASETS[j]}, model=${model}, expt=${experiment_name}"
-    continue
-  fi
-
-
-  # args:
-  # $1 = model name
-  # $2 = dataset name
-  # $3 = env name
-  # $4 = instance name
-  # $5 = experiment name
   echo "MODEL: ${model}"
   echo "ENV: ${env}"
   echo "DATASET: ${DATASETS[j]}"
   echo "EXPERIMENT_NAME: ${experiment_name}"
   #echo "tabzilla_experiment.py --experiment_config ${config_file} --dataset_dir datasets/${DATASETS[j]} --model_name MotherNet "
-  python tabzilla_experiment.py --experiment_config ${config_file} --dataset_dir datasets/${DATASETS[j]} --model_name MotherNet
+  python ../../TabZilla/tabzilla_experiment.py --experiment_config ${config_file} --dataset_dir datasets/${DATASETS[j]} --model_name MotherNet
   mv results/default_trial0_results.json results/results_${DATASETS[j]}_mothernet.json
 
   # >> ${LOG_DIR}/log_${i}_${j}_$(date +"%m%d%y_%H%M%S").txt 2>&1 &                                                                                                   
