db2e75bcec9b36aa1297ebd6450881c84e801775
Andreas Mueller <amueller@microsoft.com>
2024-03-26 18:30:44 -0700

    don't try to go parallel on GPU

--- TabZilla/models/mothernet.py
+++ TabZilla/models/mothernet.py
@@ -8,15 +8,17 @@ class MotherNet(BaseModel):
         super().__init__(params, args)
         if args.use_gpu:
             device = f"cuda:{args.gpu_ids[0]}"
+            n_jobs = 1
         else:
             device = "cpu"
             torch.set_num_threads(4)
+            n_jobs = 3
         if args.objective == "regression":
             raise NotImplementedError("Does not support")
         elif args.objective == "classification":
-            self.model = EnsembleMeta(MotherNetClassifier(device=device), n_estimators=3)
+            self.model = EnsembleMeta(MotherNetClassifier(device=device), n_estimators=3, n_jobs=n_jobs)
         elif args.objective == "binary":
-            self.model = EnsembleMeta(MotherNetClassifier(device=device), n_estimators=3)
+            self.model = EnsembleMeta(MotherNetClassifier(device=device), n_estimators=3, n_jobs=n_jobs)
 
     def fit(self, X, y, X_val=None, y_val=None):
         self.model.fit(X, y)
